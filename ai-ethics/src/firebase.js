b9d0b57ef6f6d0a66127b88b928b0b2dd0e15e29
Larimer, Josiah <josiahlarimer@my.unt.edu>
2024-09-17 15:20:26 -0500

    moved firebase creds to .env

--- ai-ethics/src/firebase.js
+++ ai-ethics/src/firebase.js
@@ -23,17 +23,15 @@ import {
   addDoc,
 } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+// the firebase creds as pulled from .env file
 const firebaseConfig = {
-  apiKey: "AIzaSyBN4IuBr5w1Qyar6H4qHZjeaEEqaJr_Lo4",
-  authDomain: "ai-ethics-4901.firebaseapp.com",
-  projectId: "ai-ethics-4901",
-  storageBucket: "ai-ethics-4901.appspot.com",
-  messagingSenderId: "953629317280",
-  appId: "1:953629317280:web:d91f986f9a2f99c617d1c5",
-  measurementId: "G-MN0KF05TSE",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
