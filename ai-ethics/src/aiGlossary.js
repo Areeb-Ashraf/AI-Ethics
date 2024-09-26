3673001ff513df6d8998797380d0c10732807380
Chase Richardson <ChaseARichardson2003@gmail.com>
2024-09-26 11:56:21 -0500

    No change in function, just preparing

--- ai-ethics/src/aiGlossary.js
+++ ai-ethics/src/aiGlossary.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "./styles/aiGlossary.css";
+import databaseManager from "./databaseManager";
 
 // Sample AI Glossary Data (to be fetched from firebase)
 const glossaryData = [
