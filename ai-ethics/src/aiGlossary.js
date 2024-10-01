13cbbc4c59eb7e99902ddcc8c482ebd1b242d14d
ChaseRichardson <ChaseARichardson2003@gmail.com>
2024-10-01 01:53:30 -0500

    Added experiment with popup, not finished

--- ai-ethics/src/aiGlossary.js
+++ ai-ethics/src/aiGlossary.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import "./styles/aiGlossary.css";
 import databaseManager from "./databaseManager";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 // Sample AI Glossary Data (to be fetched from firebase)
 const glossaryData = await databaseManager.fetchAllGlossary();
@@ -16,15 +18,44 @@ const AIGlossary = () => {
     setFilteredTitles(results);
   }, [searchTitle]);
 
+  async function fetchGlossary(word) {
+    const glossaryWord = await databaseManager.fetchGlossary(word);
+    return glossaryWord;
+  }
+
+  const Tooltip = () => {
+    const [definition, setDefinition] = useState("Loading...");
+  
+    useEffect(() => {
+      // Fetch the definition of "API" when the component mounts
+      fetchGlossary("Machine Learning").then((result) => {
+        setDefinition(result);
+      });
+    }, []); 
+  
+    return (
+      <Popup
+        trigger={open => (
+          <button className="button">Trigger - {open ? 'Opened' : 'Closed'}</button>
+        )}
+        position="right center"
+        closeOnDocumentClick
+      >
+        <span>{definition}</span> {/* This will display the fetched definition */}
+      </Popup>
+    );
+  };
+    
   return (
     <div className="glossary-container">
       <h1 className="glossary-heading">AI Glossary</h1>
+      <Tooltip />
 
       {/* Search Bar */}
-      <input 
-        type="text" 
-        className="glossary-search-bar" 
-        placeholder="Search for a term..." 
+      <input
+        type="text"
+        className="glossary-search-bar"
+        placeholder="Search for a term..."
         value={searchTitle}
         onChange={e => setSearchTitle(e.target.value)}
       />
