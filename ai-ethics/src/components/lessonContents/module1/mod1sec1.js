6c29b7f6bfecd0013bc499eaad91bbae36249fea
Ashraf, Areeb <areebashraf@my.unt.edu>
2024-10-19 20:01:34 -0500

    module template

---
+++ ai-ethics/src/components/lessonContents/module1/mod1sec1.js
@@ -0,0 +1,15 @@
+// lessonContents/Module1Lesson.js
+import React from 'react';
+import 'mod1sec1.css'
+
+const Mod1Sec1 = () => {
+    // js goes here
+  return (
+    <>
+      <h2>Module 1 - Lesson</h2>
+      <p>This is the lesson content for Module 1.</p>
+    </>
+  );
+};
+
+export default Mod1Sec1;
